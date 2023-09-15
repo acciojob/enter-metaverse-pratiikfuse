e067b8585973371b23ce125b14d60fe257f3de7d
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-15 15:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your JS code here. If required.
 function change(){
 	let pElement = document.getElementById('status');
-	pElement.remove();
 	let h1Element = document.createElement('h1');
+	h1Element.innerHTML = "Entered Metaverse"
+	pElement.innerHTML="";
+	pElement.appendChild(h1Element);
+	// pElement.remove();
+	// let h1Element = document.createElement('h1');
 
-	h1Element.innerHTML = "Entered Metaverse";
-	h1Element.id = "status";
+	// h1Element.innerHTML = "Entered Metaverse";
+	// h1Element.id = "status";
 
-	document.body.insertBefore(h1Element,document.getElementById('enterBtn'));
+	// document.body.insertBefore(h1Element,document.getElementById('enterBtn'));
 }
