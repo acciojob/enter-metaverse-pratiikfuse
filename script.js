7cb66ff32b9a23a9277d1fcd04ae7d4d21931b40
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-15 15:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function change(){
 	let h1Element = document.createElement('h1');
 
 	h1Element.innerHTML = "Entered Metaverse";
+	h1Element.id = "status";
 
 	document.body.insertBefore(h1Element,document.getElementById('enterBtn'));
 }
